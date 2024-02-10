e4830f5085b5d301336b73034db56c4caa8f09e5
Arnab-1097 <85272127+Arnab-1097@users.noreply.github.com>
2024-02-10 20:16:50 +0600

    Update Amazon Automation.py

--- Amazon Automation.py
+++ Amazon Automation.py
@@ -6,37 +6,20 @@ from selenium.webdriver.common.by import By
 #from selenium.webdriver.support.select import Select
 
 driver=webdriver.Chrome("C:/Users/88017/Desktop/Random/amazon automation/chromedriver.exe")
+
 driver.get("https://www.amazon.com/")
 time.sleep(10)
-
-#Test case 01
-
 element1=driver.find_element(By.XPATH,"//form[@id='nav-search-bar-form']//div[@class='nav-left']")
 element1.click()
-
-
 element2=driver.find_element(By.XPATH,"//option[@value='search-alias=baby-products-intl-ship']")
 time.sleep(3)
 element2.click()
-
-
-#Test case 02
-
 element3=driver.find_element(By.XPATH,"//form[@id='nav-search-bar-form']//div[@class='nav-fill']")
 element3.click()
 time.sleep(3)
-
-
 element4=driver.find_element(By.XPATH,"//input[@id='twotabsearchtextbox']")
 element4.send_keys("bag")
-
-
 element5=driver.find_element(By.XPATH,"//form[@id='nav-search-bar-form']//div[@class='nav-right']")
 element5.click()
-
-
 time.sleep(3)
-
-
-#Test case 03
 driver. close()
